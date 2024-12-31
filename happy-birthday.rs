e6059b44c05293be6bbbcc33aec0bfdbfacc1a2d
impl Ferris <167990244+ImplFerris@users.noreply.github.com>
2024-12-31 22:44:23 +0530

    Create happy-birthday.rs

---
+++ happy-birthday.rs
@@ -0,0 +1,32 @@
+use crate::music::*;
+
+pub const TEMPO: u16 = 140;
+
+// Happy Birthday
+pub const MELODY: [(f64, i16); 25] = [
+    (NOTE_C4, 4),
+    (NOTE_C4, 8),
+    (NOTE_D4, -4),
+    (NOTE_C4, -4),
+    (NOTE_F4, -4),
+    (NOTE_E4, -2),
+    (NOTE_C4, 4),
+    (NOTE_C4, 8),
+    (NOTE_D4, -4),
+    (NOTE_C4, -4),
+    (NOTE_G4, -4),
+    (NOTE_F4, -2),
+    (NOTE_C4, 4),
+    (NOTE_C4, 8),
+    (NOTE_C5, -4),
+    (NOTE_A4, -4),
+    (NOTE_F4, -4),
+    (NOTE_E4, -4),
+    (NOTE_D4, -4),
+    (NOTE_AS4, 4),
+    (NOTE_AS4, 8),
+    (NOTE_A4, -4),
+    (NOTE_F4, -4),
+    (NOTE_G4, -4),
+    (NOTE_F4, -2),
+];
